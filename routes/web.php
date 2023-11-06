45d4ee26fc1a2402fc02254938bd0f2d38ff8a90
Osas997 <larasatyt83@gmail.com>
2023-11-06 19:24:36 +0700

    auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthContoller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware("sudahlogin")->group(function () {
+    Route::get("/login", [AuthController::class, "login"])->name("login");
+    Route::post("/login", [AuthController::class, "authenticate"]);
 });
