0c74f665e2ec95d2a64c58b3ad9cdb8b83dc08ed
Osas997 <larasatyt83@gmail.com>
2023-11-09 18:11:03 +0700

    sign murid logic

--- app/Http/Controllers/MuridController.php
+++ app/Http/Controllers/MuridController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Murid;
 use Illuminate\Http\Request;
 
 class MuridController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            "nisn" => "required",
+            "nama_murid" => "required",
+            "kelas" => "required",
+            "jenis_kelamin:" => "required",
+            "id_sekolah" => auth("sekolah")->user()->id,
+        ]);
+
+        $murid = Murid::create($validate);
+        $request->session()->put('murid', $murid->id);
+        return redirect()->intended("/murid/survey");
+    }
 }
